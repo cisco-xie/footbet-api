f5e402207804407c96d9e7cd034abe6dea136714
cisco <1264108131@qq.com>
2025-02-08 22:54:03 +0800

    :sparkles: feat: 优化selenium技术抓取注单页面

--- src/main/java/com/example/demo/config/WebDriverConfig.java
+++ src/main/java/com/example/demo/config/WebDriverConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import jakarta.annotation.PreDestroy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,7 +20,7 @@ public class WebDriverConfig {
     public WebDriver webDriver() {
         // 检查当前线程是否已经有 WebDriver 实例
         if (driverThreadLocal.get() == null) {
-            System.setProperty("webdriver.chrome.driver", "D:\\developer\\chromedriver-v131.204\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "D:\\developer\\chromedriver-v133\\chromedriver.exe");
             // 配置Chrome选项以启用无头模式
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
@@ -30,10 +31,23 @@ public class WebDriverConfig {
             options.addArguments("--remote-allow-origins=*");
             options.addArguments("--blink-settings=imagesEnabled=false"); // 禁止加载图片
             WebDriver driver = new ChromeDriver(options);
+            // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             // 将 WebDriver 实例存储在 ThreadLocal 中
             driverThreadLocal.set(driver);
         }
         // 返回当前线程的 WebDriver 实例
         return driverThreadLocal.get();
     }
+
+    /**
+     * 服务关闭时自动清理 WebDriver 实例
+     */
+    @PreDestroy
+    public void cleanup() {
+        if (driverThreadLocal.get() != null) {
+            driverThreadLocal.get().quit(); // 关闭 WebDriver 实例，释放资源
+            driverThreadLocal.remove();     // 清除线程局部存储
+        }
+    }
+
 }
--- src/main/java/com/example/demo/controller/ProxyController.java
+++ src/main/java/com/example/demo/controller/ProxyController.java
@@ -13,13 +13,16 @@ import jakarta.annotation.Resource;
 import org.htmlunit.BrowserVersion;
 import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlPage;
-import org.htmlunit.util.Cookie;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -30,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
@@ -43,49 +47,96 @@ public class ProxyController extends BaseController {
     @Resource
     private ConfigAccountService accountService;
 
+    private String buildCookie(ConfigAccountVO account) {
+        String custid = account.getToken().getJSONObject("tokens").getStr("X-Custid");
+        String browser = account.getToken().getJSONObject("tokens").getStr("X-Browser-Session-Id");
+        String slid = account.getToken().getJSONObject("tokens").getStr("X-SLID");
+        String lcu = account.getToken().getJSONObject("tokens").getStr("X-Lcu");
+        return "pctag=48737fbc-cfb0-4199-b54b-32a6a57fc64e; dgRH0=6Zn5gZ2NOAamUly; skin=ps3838; b-user-id=cea40892-825f-666a-cf0e-a8fe24c39a01; _gid=GA1.2.1677352228.1736944373; _ga=GA1.2.1445030965.1736944373; PCTR=1896783056883; u=" + lcu + "; lcu=" + lcu + "; custid=" + custid + "; BrowserSessionId=" + browser + "; _og=QQ==; _ulp=KzhkT2JESFJ1US9xbC9rZkxDaHJZb3V2YVZlTCtKN2ViYnBYdGNCY0U2SzB4bnZpTVZaQWVwamhPQW5FSkNqS3xiOGZmZmEzZGNlM2Y0MGJiMmRlNDE2ZTEwYTMzMWM3Mg==; uoc=be97830afef253f33d2a502d243b8c37; _userDefaultView=COMPACT; SLID=" + slid + "; auth=true; _sig=Icy1OV014TnpZeVl6RTROek0wTXpjNE5nOm5xd2hWTTZTdmJIVmluQ0k1TndvaWxMS2g6MTcxMzE1ODI0NDo3Mzc2OTk5MTY6bm9uZTo5Q0NFQTlvSVhE; _apt=9CCEA9oIXD; _ga_DXNRHBHDY9=GS1.1.1736944373.1.1.1736944383.50.0.1813848857; _ga_1YEJQEHQ55=GS1.1.1739016699.1.0.1739016745.14.0.0; _vid=3cdc158d8a079b8caa05594a23644c6d; __prefs=W251bGwsNCwxLDAsMSxudWxsLGZhbHNlLDAuMDAwMCx0cnVlLHRydWUsIl8zTElORVMiLDAsbnVsbCx0cnVlLHRydWUsZmFsc2UsZmFsc2UsbnVsbCxudWxsLHRydWVd; lang=zh_CN; _lastView=eyJoNjEwMDAwMDAxIjoiQ09NUEFDVCJ9";
+    }
+
+    private void setCookies(WebDriver driver, String cookie) {
+        StringTokenizer st = new StringTokenizer(cookie, ";");
+        driver.get("https://www.ps3838.com");
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken().trim();
+            int idx = token.indexOf("=");
+            if (idx > 0) {
+                String name = token.substring(0, idx);
+                String value = token.substring(idx + 1);
+                Cookie seleniumCookie = new Cookie.Builder(name, value)
+                        .domain(".ps3838.com")
+                        .path("/")
+                        .isSecure(true)  // 设置为 Secure，确保 HTTPS 环境下有效
+                        .sameSite("None")  // 允许跨域 Cookie
+                        .build();
+                driver.manage().addCookie(seleniumCookie);
+            }
+        }
+    }
+
     @GetMapping("/selenium")
     public String proxyselenium(@RequestParam String url, @RequestParam String websiteId, @RequestParam String accountId) throws Exception {
         WebDriver driver = webDriver; // 从配置中获取共享实例
+
         try {
             AdminLoginDTO admin = getUser();
             ConfigAccountVO account = accountService.getAccountById(admin.getUsername(), websiteId, accountId);
 
             // 构建完整的cookie字符串
-            String custid = account.getToken().getJSONObject("tokens").getStr("X-Custid");
-            String browser = account.getToken().getJSONObject("tokens").getStr("X-Browser-Session-Id");
-            String slid = account.getToken().getJSONObject("tokens").getStr("X-SLID");
-            String lcu = account.getToken().getJSONObject("tokens").getStr("X-Lcu");
-            String cookie = "JSESSIONID=60080FF6CE15EA2EAE4212CCBE25C58E; pctag=48737fbc-cfb0-4199-b54b-32a6a57fc64e; dgRH0=6Zn5gZ2NOAamUly; skin=ps3838; b-user-id=86848c3d-24b8-fa15-e0c4-c26ae9df3b9a; _gid=GA1.2.1677352228.1736944373; _ga=GA1.2.1445030965.1736944373; PCTR=1894710782467; u=" + lcu + "; lcu=" + lcu + "; custid=" + custid + "; BrowserSessionId=" + browser + "; _og=QQ==; _ulp=KzhkT2JESFJ1US9xbC9rZkxDaHJZb3V2YVZlTCtKN2ViYnBYdGNCY0U2SzB4bnZpTVZaQWVwamhPQW5FSkNqS3xiOGZmZmEzZGNlM2Y0MGJiMmRlNDE2ZTEwYTMzMWM3Mg==; uoc=be97830afef253f33d2a502d243b8c37; _userDefaultView=COMPACT; SLID=" + slid + "; _sig=Tcy1OV014TnpZeVl6RTROek0wTXpjNE5nOjNjZWtOQmp0eUczZGhEVE5TcHZzYWVHRmU6MTcxMzE1ODI0NDo3MzY5NDQzODI6bm9uZTpXb2U1NlZ6M3Uw; _apt=Woe56Vz3u0; _ga_DXNRHBHDY9=GS1.1.1736944373.1.1.1736944383.50.0.1813848857; _ga_1YEJQEHQ55=GS1.1.1736944373.1.1.1736944383.50.0.0; _vid=dde4ede6a2ad88833c20148ab7cecb52; __prefs=W251bGwsMSwxLDAsMSxudWxsLGZhbHNlLDAuMDAwMCxmYWxzZSx0cnVlLCJfM0xJTkVTIiwxLG51bGwsdHJ1ZSx0cnVlLGZhbHNlLGZhbHNlLG51bGwsbnVsbCx0cnVlXQ==; lang=zh_CN; _lastView=eyJoNjEwMDAwMDAxIjoiQ09NUEFDVCJ9";
+            String cookie = buildCookie(account);
 
-            // 分割cookie字符串并添加到WebDriver
-            StringTokenizer st = new StringTokenizer(cookie, ";");
-            driver.get("https://www.ps3838.com");
-            while (st.hasMoreTokens()) {
-                String token = st.nextToken().trim();
-                int idx = token.indexOf("=");
-                if (idx > 0) {
-                    String name = token.substring(0, idx);
-                    String value = token.substring(idx + 1);
-                    org.openqa.selenium.Cookie seleniumCookie = new org.openqa.selenium.Cookie.Builder(name, value)
-                            .domain(".ps3838.com")
-                            .path("/")
-                            .build();
-                    driver.manage().addCookie(seleniumCookie);
+            // 设置 Cookie
+            setCookies(driver, cookie);
+
+            // 导航到目标页面并等待加载完成
+            int retries = 3;
+            while (retries > 0) {
+                try {
+                    driver.get(url);
+                    waitForPageToLoad(driver);
+                    break; // 成功则退出循环
+                } catch (TimeoutException e) {
+                    retries--;
+                    if (retries == 0) {
+                        throw new RuntimeException("页面加载超时，重试次数用尽", e);
+                    }
+                    System.out.println("页面加载超时，剩余重试次数: " + retries);
                 }
             }
-
-            // 导航到目标网页
-            driver.get(url);
-            // 等待页面加载完成，这里使用Thread.sleep，也可以使用WebDriver的显式等待
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("body")));
-
             // 获取页面源代码
-            return driver.getPageSource().replaceAll("href=\"/", "href=\"https://www.ps3838.com/");
+            String pageSource = driver.getPageSource();
+
+            // 解析 HTML
+            Document document = Jsoup.parse(pageSource);
+
+            String baseUrl = "https://www.ps3838.com";
+            // 在 <head> 中插入 <base> 标签
+            // document.head().prepend("<base href=\""+baseUrl+"\">");
+
+            // 选择所有 class 为 "form-inline" 的 form 元素(这个是查询表单块)，并删除
+            document.select("form.form-inline").remove();
+            // 删除所有 class 为 "truncated-currencies" 的 <div> 元素
+            document.select("div.truncated-currencies").remove();
+
+            // 修正资源路径
+            document.select("link[href^='/'], script[src^='/'], img[src^='/']").forEach(element -> {
+                if (element.hasAttr("href")) {
+                    element.attr("href", baseUrl + element.attr("href"));
+                }
+            });
+            // 获取页面源代码
+             return document.html();
         } finally {
             // 关闭WebDriver
             // driver.quit();
         }
+    }
+
+    private void waitForPageToLoad(WebDriver driver) {
+        // 使用一个显式等待条件，直到出现 tbody 标签 为止
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("tbody")));
     }
 
 }
